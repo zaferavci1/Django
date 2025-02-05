afa82472189ef2c588fddea8794a4bc471f5a19c
zaferavci1 <zaferavci89@gmail.com>
2025-02-05 18:40:02 +0300

    reupdate

--- course/urls.py
+++ course/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("anasayfa", include("courses.urls")),
+    path("", include("courses.urls")),
     path('admin/', admin.site.urls),
 ]
